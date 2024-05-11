892d234147cd5740be6d46fed84cd1094048acc7
Marina <marinabravo94@gmail.com>
2024-05-12 01:09:06 +0200

    rediseñar

---
+++ Javascript/ejercicio1.js
@@ -0,0 +1,6 @@
+// Ejercicio 1
+let precio = 200;
+let iva = 21;
+let impuesto = (21/100*200);
+let total = precio+impuesto;
+document.getElementById ("resultado").innerHTML = total;
---
+++ Javascript/practicas.js
@@ -0,0 +1,2 @@
+document.getElementById ("casa").innerHTML = 5 + 6;
+document.getElementById ("arbol").innerHTML = 200 + 42;
