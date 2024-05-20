c8f394c02bf668fbd5509da0cbd987aef008d601
Marina <marinabravo94@gmail.com>
2024-05-20 20:30:46 +0200

    length

---
+++ Javapractica/operador_comparacion3.js
@@ -0,0 +1,8 @@
+let string = "Mi nombre es Marina y tengo 30 años";
+let length = string.length;
+let start = string.startsWith("M");
+let Length = string.length > 5;
+
+document.getElementById("strings").innerHTML= + length;
+document.getElementById("empezar").innerHTML= + start;
+document.getElementById("length").innerHTML= + Length;
