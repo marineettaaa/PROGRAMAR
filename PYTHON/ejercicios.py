f582bdf703a991f85a1095626b2bcc1588c2f732
Marina <166524088+mbravo-94@users.noreply.github.com>
2024-06-06 19:18:26 +0200

    Add files via upload

---
+++ PYTHON/ejercicios.py
@@ -0,0 +1,26 @@
+
+# %%
+print("Hola")
+# %%
+
+#Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
+
+# %%
+def calcular_maximo(a:int,b:int) ->int:
+    if a > b:
+        return a
+    else:
+        return b
+    
+r_1 = calcular_maximo(1,2)
+r_2 = calcular_maximo(50,80)
+
+assert r_1 == 2
+
+print (r_1)
+
+#Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
+# %%
+def calcular_maximo_de_tres (a:int,b:int,c:int) ->int:
+    if a > b and a > c:
+        return a
